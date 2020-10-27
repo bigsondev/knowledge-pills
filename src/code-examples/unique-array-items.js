c2e8c50b2b68c0a2e55322073279a56aa4228cc1
Adrian Bigaj <adrian.bigaj@clxcommunications.com>
2020-10-27 17:29:32 +0100

    Shorten the code

--- src/code-examples/unique-array-items.js
+++ src/code-examples/unique-array-items.js
@@ -2,15 +2,15 @@ const animals = ['dog', 'cat', 'bird', 'dog', 'bird', 'elephant', 'duck', 'duck'
 
 // 1. Using Set (collection of unique values) + spread operator
 const uniqueAnimals = [...new Set(animals)];
-console.log(uniqueAnimals); // -> ["dog", "cat", "bird", "elephant", "duck"]
+console.log(uniqueAnimals); // ["dog", "cat", "bird", "elephant", "duck"]
 
 // 2. Using a filter method to exclude duplicated animals
 const uniqueAnimals = animals.filter(
   (animal, index, animals) => animals.indexOf(animal) === index
 );
-console.log(uniqueAnimals); // -> ["dog", "cat", "bird", "elephant", "duck"]
+console.log(uniqueAnimals); // ["dog", "cat", "bird", "elephant", "duck"]
 
-// 3. Using a reduce for object of unique key value pairs, then Object.keys or Object.values
+// 3. Using a reduce, then Object.keys or Object.values
 const uniqueAnimals = Object.keys(
   animals.reduce((accumulator, animal) => {
     accumulator[animal] = animal;
@@ -18,4 +18,4 @@ const uniqueAnimals = Object.keys(
     return accumulator;
   }, {})
 );
-console.log(uniqueAnimals); // -> ["dog", "cat", "bird", "elephant", "duck"]
+console.log(uniqueAnimals); // ["dog", "cat", "bird", "elephant", "duck"]
